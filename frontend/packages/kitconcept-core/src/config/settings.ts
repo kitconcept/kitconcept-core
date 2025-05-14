cf6975847efc172d23a76144574cc22c0f5835bf
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-05-14 12:06:15 +0200

    Adjustments in the frontend setup

--- frontend/packages/kitconcept-core/src/config/settings.ts
+++ frontend/packages/kitconcept-core/src/config/settings.ts
@@ -3,6 +3,7 @@ import type { ConfigType } from '@plone/registry';
 export default function install(config: ConfigType) {
   // Language is Deutsch
   config.settings.defaultLanguage = 'de';
+  config.settings.supportedLanguages = ['en', 'de'];
 
   return config;
 }
