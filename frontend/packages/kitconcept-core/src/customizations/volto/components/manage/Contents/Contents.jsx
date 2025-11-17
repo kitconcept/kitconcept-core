4ec3955411bebc09d6939cbbf465662bbb478215
Tishasoumya-02 <tishasoumya@gmail.com>
2025-11-17 12:06:50 +0530

    add customization docs

--- frontend/packages/kitconcept-core/src/customizations/volto/components/manage/Contents/Contents.jsx
+++ frontend/packages/kitconcept-core/src/customizations/volto/components/manage/Contents/Contents.jsx
@@ -1,13 +1,17 @@
 /**
  * OVERRIDE Contents.jsx
- * REASON: Add component DropzoneContent to enable drag and drop upload
+ * REASON: Add component DropzoneContent to enable drag and drop upload.
+ * PULL REQUEST: https://github.com/kitconcept/kitconcept-core/pull/71
+ * TICKET: https://gitlab.kitconcept.io/kitconcept/distribution-kitconcept-intranet/-/issues/6
+ * DEVELOPER: @Tishasoumya-02
+ * CHANGELOG:
+ *  - Add component DropzoneContent to enable drag and drop upload (#6) @Tishasoumya-02
  */
 
 /**
  * Contents component.
  * @module components/manage/Contents/Contents
  */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
