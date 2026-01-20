ecad4c99c6d3246d532817f955c0cccdef20eac3
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-01-20 11:37:18 +0100

    Move k.footer to core

--- frontend/packages/kitconcept-core/src/config/slots.ts
+++ frontend/packages/kitconcept-core/src/config/slots.ts
@@ -1,6 +1,13 @@
 import type { ConfigType } from '@plone/registry';
 import TTWCustomCSS from '../slots/TTWCustomCSS/TTWCustomCSS';
 import ConfigInjector from '../slots/ConfigInjector/ConfigInjector';
+import type { Content } from '@plone/types';
+import CoreFooter from '../slots/Footer/CoreFooter';
+
+export function hasInheritedBehavior(behavior: string) {
+  return ({ content }: { content: Content }) =>
+    Object.keys(content?.['@components']?.inherit || {}).includes(behavior);
+}
 
 export default function install(config: ConfigType) {
   config.registerSlotComponent({
@@ -13,6 +20,13 @@ export default function install(config: ConfigType) {
     slot: 'aboveHeader',
     name: 'TTWCustomCSS',
     component: TTWCustomCSS,
+  });
+
+  config.registerSlotComponent({
+    name: 'coreFooter',
+    slot: 'footer',
+    component: CoreFooter,
+    predicates: [hasInheritedBehavior('kitconcept.footer')],
   });
 
   return config;
