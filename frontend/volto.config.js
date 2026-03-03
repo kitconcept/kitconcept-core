bbeed897906fce9e8a726e47f372b25ae3788fef
iFlameing <ialokkumarsingh0@gmail.com>
2026-03-03 19:40:37 +0530

    first iteration of adding backend of volto-contact-block

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,4 +1,8 @@
-const addons = ['@kitconcept/core', '@kitconcept/volto-light-theme'];
+const addons = [
+  '@kitconcept/core',
+  '@kitconcept/volto-light-theme',
+  '@kitconcept/volto-contact-block',
+];
 const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
