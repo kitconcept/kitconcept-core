77f80ab6b46e50f66bfb320d0d8a21ca919ab8fb
Víctor Fernández de Alba <sneridagh@gmail.com>
2026-09-17 13:30:58 +0200

    Update Volto and VLT (#138)

--- backend/tests/tools/test_migration_tool.py
+++ backend/tests/tools/test_migration_tool.py
@@ -57,8 +57,8 @@ class TestMigrationToolVersions:
                     "fs_version": "current_profile_version",
                 },
             ],
-            ["Zope", "6.1"],
-            ["CMFPlone", "6.2.1"],
+            ["Zope", "6.2"],
+            ["CMFPlone", "6.2.2"],
         ],
     )
     def test_coreVersions(self, key: str, expected: str):
